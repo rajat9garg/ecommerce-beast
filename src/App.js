5476bcc63fb5447d7a4b941daf46c0638504439b
rajat9garg <rajat9garg@gmail.com>
2019-07-21 14:12:09 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/hompage.component'
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
     </div>
   );
 }
